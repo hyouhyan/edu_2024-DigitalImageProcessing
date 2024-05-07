74f04cf2ea8e42a93931d83ce65d3cfec7781638
Hyouhyan <hyouhyan@hyouhyan.com>
2024-05-07 13:27:03 +0900

    動画像の保存に対応

--- 03_2024-05-07/dip03.cpp
+++ 03_2024-05-07/dip03.cpp
@@ -24,11 +24,17 @@ int main (int argc, const char* argv[])
     cv::Mat frameImage = cv::Mat(cv::Size(width, height), CV_8UC3);
     cv::Mat grayImage;
 
+    //ファイル出力用
+    cv::Mat recImage == cv::Mat(cv::Size(width, height), CV_8UC3);
+
     //ウィンドウの生成と移動
     cv::namedWindow("Frame");
     cv::moveWindow("Frame", 0,0);
     cv::namedWindow("Result");
     cv::moveWindow("Result", 0,height);
+
+    //ビデオライタ生成
+    cv::VideoWriter rec("rec.mpg", cv::VideoWriter::fourcc('P','I','M','1'), 30, recImage.size());
 
     //動画処理無限ループ
     while(1){
