7bced024c59e3eaae753f02b364f2f2bfae71bcf
Hyouhyan <hyouhyan@hyouhyan.com>
2024-05-21 14:59:23 +0900

    課題2完成

--- 05_2024-05-21/kadai2.cpp
+++ 05_2024-05-21/kadai2.cpp
@@ -39,10 +39,11 @@ int main(int argc, char *argv[]) {
     cv::Mat frameImage;
     cv::Mat originalImage(cv::Size(width, height), CV_8UC3);
     cv::Mat grayImage(cv::Size(width, height), CV_8UC1);
-    cv::Mat resultImage(cv::Size(width, height), CV_8UC1);
-    cv::Mat kernelImage(cv::Size(width, height), CV_8UC1);
+    cv::Mat resultImage(cv::Size(width, height), CV_8UC3);
     cv::Mat lapImage(cv::Size(width, height), CV_8UC1);
     cv::Mat recImage(cv::Size(width, height), CV_8UC3);
+    cv::Mat animeImage(cv::Size(width, height), CV_8UC3);
+    cv::Mat binImage(cv::Size(width, height), CV_8UC1);
 
     //画像表示用ウィンドウの生成
     cv::namedWindow("Original");
@@ -51,8 +52,6 @@ int main(int argc, char *argv[]) {
     cv::moveWindow("Gray", 0, 200);
     cv::namedWindow("Result");
     cv::moveWindow("Result", 200, 200);
-    cv::namedWindow("Kernel");
-    cv::moveWindow("Kernel", 200, 50);
 
     //線形空間フィルタの生成
     //線形空間フィルタの係数を要素とする1次元配列の作成(5x5の平均値フィルタ用)
@@ -66,7 +65,7 @@ int main(int argc, char *argv[]) {
     //一次元配列の要素に基づき，線形空間フィルタをCvMat型の5x5行列"kernel"として生成
     cv::Mat kernel(cv::Size(5, 5), CV_32F, fdata);
 
-    cv::VideoWriter rec("dip05_kadai1.mp4", cv::VideoWriter::fourcc('H','2','6','4'), 30, recImage.size());
+    cv::VideoWriter rec("dip05_kadai2.mp4", cv::VideoWriter::fourcc('H','2','6','4'), 30, recImage.size());
 
     //動画像処理無限ループ：「ビデオキャプチャから1フレーム取り込み」→「画像処理」→「表示」の繰り返し
     while (1) {
@@ -84,23 +83,38 @@ int main(int argc, char *argv[]) {
         //メディアンフィルタを適用
         cv::medianBlur(grayImage, grayImage, 3);
 
-        //"grayImage"に線形空間フィルタ"kernel"を適用
-        // cv::filter2D(grayImage, kernelImage, grayImage.depth(), kernel);
-
         //ラプラシアンフィルタを適用
         cv::Laplacian(grayImage, lapImage, CV_8U, 5);
 
         //2値化
-        cv::threshold(lapImage, resultImage, 128, 255, cv::THRESH_BINARY);
+        cv::threshold(lapImage, binImage, 128, 255, cv::THRESH_BINARY);
+
+        //アニメ調色変換
+        myAnimeColor(originalImage, animeImage);
+
+        // 画像を走査し、binImageの白画素をresultImageにコピーし、黒画素の場合は、animeImageの画素をコピー
+        for (int j = 0; j < originalImage.rows; j++) {
+            for (int i = 0; i < originalImage.cols; i++) {
+                if (binImage.at<unsigned char>(j, i) == 255) {
+                    resultImage.at<cv::Vec3b>(j, i) = cv::Vec3b(0, 0, 0);
+                } else {
+                    resultImage.at<cv::Vec3b>(j, i) = animeImage.at<cv::Vec3b>(j, i);
+                }
+            }
+        }
+
+        
+
+
 
         //ウィンドウに画像表示
         cv::imshow("Original", originalImage);
         cv::imshow("Gray", grayImage);
-        cv::imshow("Kernel", kernelImage);
         cv::imshow("Result", resultImage);
+        cv::imshow("Anime", animeImage);
 
-        cv::cvtColor(resultImage, recImage, cv::COLOR_GRAY2BGR);
-        rec << recImage;
+        // cv::cvtColor(resultImage, recImage, cv::COLOR_BGR2RGB);
+        rec << resultImage;
 
         //[q]キーが押されたら無限ループから脱出
         int key = cv::waitKey(10);
