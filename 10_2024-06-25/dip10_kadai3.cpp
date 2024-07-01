289b4ed9437c4bd5204d40353878e95ecc275f94
Hyouhyan <hyouhyan@hyouhyan.com>
2024-07-01 13:38:39 +0900

    10-3 それっぽくなった

--- 10_2024-06-25/dip10_kadai3.cpp
+++ 10_2024-06-25/dip10_kadai3.cpp
@@ -5,12 +5,8 @@
 
 // 配列の象限入れ替え用関数の宣言
 void ShiftDFT(const cv::Mat& src_arr, cv::Mat& dst_arr);
-void applyGaussianLowPassFilter(cv::Mat& ftMatrix, double sigma);
-void applyGaussianHighPassFilter(cv::Mat& ftMatrix, double sigma);
-cv::Mat calculateMagnitude(const cv::Mat& complexImg);
 
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char* argv[]){
     // 画像の読み込み
     cv::Mat sourceImg = cv::imread("./src/nymegami.jpg", cv::IMREAD_GRAYSCALE);
     // cv::Mat sourceImg = cv::imread("./src/lenna_g.jpg", cv::IMREAD_GRAYSCALE);
@@ -23,9 +19,12 @@ int main(int argc, const char* argv[])
     cv::imwrite("./dst/dip10_kadai3/original.jpg", sourceImg);
 
     // 作業用配列領域の宣言
-    cv::Mat cxMatrix(sourceImg.size(), CV_64FC2); // 複素数用(実数 2 チャンネル)
-    cv::Mat ftMatrix(sourceImg.size(), CV_64FC2); // 複素数用(実数 2 チャンネル)
-    cv::Mat resultImg(sourceImg.size(), CV_8UC1); // 逆変換画像(自然数)
+    cv::Mat cxMatrix(sourceImg.size(), CV_64FC2); //複素数用(実数 2 チャンネル)
+    cv::Mat ftMatrix(sourceImg.size(), CV_64FC2); //複素数用(実数 2 チャンネル)
+    cv::Mat spcMatrix(sourceImg.size(), CV_64FC1); //スペルトルデータ(実数)
+    
+    cv::Mat spcImg(sourceImg.size(), CV_8UC1); //スペクトル画像(自然数)
+    cv::Mat resultImg(sourceImg.size(), CV_8UC1); //逆変換画像(自然数)
 
     // 原画像を複素数(実数部と虚数部)の 2 チャンネル配列として表現．虚数部はゼロ
     cv::Mat imgMatrix[] = {cv::Mat_<double>(sourceImg), cv::Mat::zeros(sourceImg.size(),CV_64FC1)};
@@ -36,32 +35,35 @@ int main(int argc, const char* argv[])
     cv::dft(cxMatrix, ftMatrix);
     ShiftDFT(ftMatrix, ftMatrix);
 
-    // ローパスフィルタの適用
-    double sigma = 30.0;
-    applyGaussianLowPassFilter(ftMatrix, sigma);
-    cv::Mat lowPassFreqImg = calculateMagnitude(ftMatrix);
-    cv::imwrite("./dst/dip10_kadai3/lowpass_freq.jpg", lowPassFreqImg);
+    double sigma = 0.008;  // ガウシアンフィルタの標準偏差
+    cv::Point center2 = cv::Point(ftMatrix.cols / 2, ftMatrix.rows / 2);
+    for (int i = 0; i < ftMatrix.rows; ++i) {
+        for (int j = 0; j < ftMatrix.cols; ++j) {
+            double radius = std::pow(j - center2.x, 2.0) + std::pow(i - center2.y, 2.0);
+            // double weight =  std::exp(-2.0 * M_PI * M_PI * std::pow(radius, 2.0) / (2.0 * std::pow(sigma, 2.0)));
+            double weight =  1 - std::exp(-2.0 * M_PI * M_PI * std::pow(sigma, 2.0) * radius);
+            ftMatrix.at<cv::Vec2d>(i, j)[0] *= weight;
+            ftMatrix.at<cv::Vec2d>(i, j)[1] *= weight;
+        }
+    }
+
+    // フーリエスペクトル"spcMatrix"の計算
+    cv::split(ftMatrix, imgMatrix);
+    cv::magnitude(imgMatrix[0], imgMatrix[1], spcMatrix);
+
+    // フーリエスペクトルからフーリエスペクトル画像を生成
+    spcMatrix += cv::Scalar::all(1);
+    cv::log(spcMatrix, spcMatrix);
+    cv::normalize(spcMatrix, spcImg, 0, 255, cv::NORM_MINMAX, CV_8U);
 
     // フーリエ逆変換
     ShiftDFT(ftMatrix, ftMatrix);
     cv::idft(ftMatrix, cxMatrix);
     cv::split(cxMatrix, imgMatrix);
     cv::normalize(imgMatrix[0], resultImg, 0, 255, cv::NORM_MINMAX, CV_8U);
-    cv::imwrite("./dst/dip10_kadai3/lowpass_spatial.jpg", resultImg);
+    cv::imwrite("./dst/dip10_kadai3/spatial.jpg", spcImg);
+    cv::imwrite("./dst/dip10_kadai3/result.jpg", resultImg);
 
-    // ハイパスフィルタの適用
-    cv::dft(cxMatrix, ftMatrix);  // フーリエ変換し直す
-    ShiftDFT(ftMatrix, ftMatrix);
-    applyGaussianHighPassFilter(ftMatrix, sigma);
-    cv::Mat highPassFreqImg = calculateMagnitude(ftMatrix);
-    cv::imwrite("./dst/dip10_kadai3/highpass_freq.jpg", highPassFreqImg);
-
-    // フーリエ逆変換
-    ShiftDFT(ftMatrix, ftMatrix);
-    cv::idft(ftMatrix, cxMatrix);
-    cv::split(cxMatrix, imgMatrix);
-    cv::normalize(imgMatrix[0], resultImg, 0, 255, cv::NORM_MINMAX, CV_8U);
-    cv::imwrite("./dst/dip10_kadai3/highpass_spatial.jpg", resultImg);
 
     return 0;
 }
@@ -85,49 +87,4 @@ void ShiftDFT(const cv::Mat& src_arr, cv::Mat& dst_arr)
     q2.copyTo(tmp);
     q4.copyTo(q2);
     tmp.copyTo(q4);
-}
-
-// ガウシアンローパスフィルタの適用
-void applyGaussianLowPassFilter(cv::Mat& ftMatrix, double sigma)
-{
-    int rows = ftMatrix.rows;
-    int cols = ftMatrix.cols;
-    int centerX = cols / 2;
-    int centerY = rows / 2;
-
-    for (int i = 0; i < cols; i++) {
-        for (int j = 0; j < rows; j++) {
-            double distance = sqrt(pow(i - centerX, 2) + pow(j - centerY, 2));
-            double value = exp(-(distance * distance) / (2 * sigma * sigma));
-            ftMatrix.at<cv::Vec2d>(j, i) *= value;
-        }
-    }
-}
-
-// ガウシアンハイパスフィルタの適用
-void applyGaussianHighPassFilter(cv::Mat& ftMatrix, double sigma)
-{
-    int rows = ftMatrix.rows;
-    int cols = ftMatrix.cols;
-    int centerX = cols / 2;
-    int centerY = rows / 2;
-
-    for (int i = 0; i < cols; i++) {
-        for (int j = 0; j < rows; j++) {
-            double distance = sqrt(pow(i - centerX, 2) + pow(j - centerY, 2));
-            double value = 1.0 - exp(-(distance * distance) / (2 * sigma * sigma));
-            ftMatrix.at<cv::Vec2d>(j, i) *= value;
-        }
-    }
-}
-
-// 複素数画像の振幅（マグニチュード）を計算
-cv::Mat calculateMagnitude(const cv::Mat& complexImg)
-{
-    cv::Mat planes[2];
-    cv::split(complexImg, planes);
-    cv::Mat mag;
-    cv::magnitude(planes[0], planes[1], mag);
-    cv::normalize(mag, mag, 0, 255, cv::NORM_MINMAX, CV_8U);
-    return mag;
 }
