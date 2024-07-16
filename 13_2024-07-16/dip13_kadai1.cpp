58cb9d8705ec37f9de43023c9990766689663525
hyouhyan <hyouhyan@hyouhyan.com>
2024-07-16 14:44:11 +0900

    わっかんねえよおおおおおおおおおおお

--- 13_2024-07-16/dip13_kadai1.cpp
+++ 13_2024-07-16/dip13_kadai1.cpp
@@ -10,6 +10,13 @@ int main(int argc, char* argv[])
         printf("Capture not found\n");
         return -1;
     }
+
+    // パンの種類を表す色を定義
+    const cv::Scalar color_goro_goro(255, 255, 0); // シアン
+    const cv::Scalar color_cheese(255, 0, 0);      // 青
+    const cv::Scalar color_thick_choco(0, 255, 0); // 緑
+    const cv::Scalar color_banana_bread(255, 0, 255); // マゼンタ
+    const cv::Scalar color_lemon_baum(0, 0, 255);  // 赤
     
     //画像格納用インスタンス準備
     cv::Mat frameImage;  //ビデオキャプチャ用
@@ -22,6 +29,13 @@ int main(int argc, char* argv[])
     cv::namedWindow("Frame");
     cv::namedWindow("Binary");
     cv::namedWindow("Contour");
+
+    //オプティカルフローに関する初期設定
+    cv::Mat priorImage(imageSize, CV_8UC1);  //前フレーム画像
+    cv::TermCriteria criteria = cv::TermCriteria(cv::TermCriteria::MAX_ITER|cv::TermCriteria::EPS, 20, 0.05);  //反復アルゴリズム停止基準
+    std::vector<cv::Point2f> priorFeature, presentFeature;  //前フレームおよび現フレーム特徴点
+    std::vector<unsigned char> status;  //作業用
+    std::vector<float> errors;  //作業用
 
     // ビデオライタ 
     cv::VideoWriter writer("dip13_kadai1.mp4", cv::VideoWriter::fourcc('m', 'p', '4', 'v'), 30, imageSize);
@@ -40,19 +54,42 @@ int main(int argc, char* argv[])
         cv::Mat grayImage;
         cv::cvtColor(frameImage, grayImage, cv::COLOR_BGR2GRAY);
 
+        cv::goodFeaturesToTrack(grayImage, priorFeature, 300, 0.01, 10);
+
+        // オプティカルフロー計算
+        int opCnt = priorFeature.size(); //特徴点数
+
+        if (opCnt > 0) {
+            cv::calcOpticalFlowPyrLK(priorImage, grayImage, priorFeature, presentFeature, status, errors, cv::Size(10, 10), 3, criteria);
+
+            // オプティカルフローの平均を計算
+            cv::Point2f sum(0, 0);
+            for (int i = 0; i < opCnt; i++) {
+                // 極端に大きな値は無視
+                if(presentFeature[i].x - priorFeature[i].x > 20 || presentFeature[i].y - priorFeature[i].y > 20) {
+                    continue;
+                }
+                sum += presentFeature[i] - priorFeature[i];
+            }
+            cv::Point2f ave = sum / opCnt;
+
+            // 画面中央に矢印を描画
+            cv::arrowedLine(frameImage, cv::Point(width / 2, height / 2), cv::Point(width / 2 + ave.x, height / 2 + ave.y), cv::Scalar(0, 255, 0), 2);
+        }
+
         // 2値化
         cv::Mat binaryImage;
-        cv::threshold(grayImage, binaryImage, 180, 255, cv::THRESH_BINARY);
+        cv::threshold(grayImage, binaryImage, 185, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
         // 白黒反転
         cv::bitwise_not(binaryImage, binaryImage);
 
         // 収縮・膨張処理
-        cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
-        cv::erode(binaryImage, binaryImage, kernel, cv::Point(-1, -1), 10);
-        cv::dilate(binaryImage, binaryImage, kernel, cv::Point(-1, -1), 10);
+        // cv::Mat kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3));
+        // cv::erode(binaryImage, binaryImage, kernel, cv::Point(-1, -1), 10);
+        // cv::dilate(binaryImage, binaryImage, kernel, cv::Point(-1, -1), 10);
 
-        cv::dilate(binaryImage, binaryImage, kernel, cv::Point(-1, -1), 10);
-        cv::erode(binaryImage, binaryImage, kernel, cv::Point(-1, -1), 10);
+        // cv::dilate(binaryImage, binaryImage, kernel, cv::Point(-1, -1), 10);
+        // cv::erode(binaryImage, binaryImage, kernel, cv::Point(-1, -1), 10);
 
 
         // 輪郭表示用画像
@@ -69,9 +106,12 @@ int main(int argc, char* argv[])
         for (int i = 0; i < contours.size(); i++) {
             // 面積が小さいものは無視
             if (cv::contourArea(contours[i]) > 1200) {
-                
-                
                 cv::drawContours(contourImage, contours, i, cv::Scalar(0, 0, 255), 2);
+
+                // 要素の面積を画面上に表示
+                cv::Moments mu = cv::moments(contours[i]);
+                cv::Point2f mc = cv::Point2f(mu.m10 / mu.m00, mu.m01 / mu.m00);
+                cv::putText(contourImage, std::to_string(cv::contourArea(contours[i])), mc, cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 2);
             }
         }
         
@@ -84,6 +124,8 @@ int main(int argc, char* argv[])
 
         //動画出力
         writer << contourImage;
+
+        priorImage = grayImage.clone();
 
         //キー入力待ち
         int key = cv::waitKey(20);
